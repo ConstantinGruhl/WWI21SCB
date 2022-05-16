056ebecb6309ac2441f8194d6b12e09736c8c9f4
Gruhl <constantin.gruhl@accenture.com>
2022-05-16 16:31:09 +0200

    Ich habe Code hinzugefügt.

---
+++ WWI21SCBGit/src/main/java/Dhruv/DhruvduLappen.java
@@ -0,0 +1,7 @@
+package Dhruv;
+
+public class DhruvduLappen {
+    public static void main(String[] args) {
+        System.out.println("Dhruv du bist ein Lappen, aber ich bin froh, dass du dem Git gejoined bist.");
+    }
+}
