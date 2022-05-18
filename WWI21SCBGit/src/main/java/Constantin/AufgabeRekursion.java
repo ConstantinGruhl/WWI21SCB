68d448c9075fe4c30c692808906edab0f4ca8c9a
Gruhl <constantin.gruhl@accenture.com>
2022-05-18 09:17:41 +0200

    kjbwpdvu

---
+++ WWI21SCBGit/src/main/java/Constantin/AufgabeRekursion.java
@@ -0,0 +1,12 @@
+package Constantin;
+
+public class AufgabeRekursion {
+
+    private static long calculateFibonacciAstleration(int n) {
+
+    }
+
+    private static long calculateFibonacciRecursion(int n){
+
+    }
+}
---
+++ WWI21SCBGit/src/main/java/Daniel/Daniel.java
@@ -0,0 +1,9 @@
+package Daniel;
+
+import java.sql.SQLOutput;
+
+public class Daniel {
+    public static void main(String[] args) {
+        System.out.println("Hallo");
+    }
+}
