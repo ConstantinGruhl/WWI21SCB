242cf959877ed0f38e35a3296b502273af41c6ec
constantin.gruhl <constantin.gruhl@gmail.com>
2022-06-13 15:56:41 +0200

    Hi

--- WWI21SCBGit/src/main/java/Constantin/Node.java
+++ WWI21SCBGit/src/main/java/Constantin/Node.java
@@ -11,3 +11,56 @@ public interface Node<T> {
 	public String toString();
 	
 }
+
+class Nodeimpl<T> implements Node{
+
+	private T wert;
+	private Node next;
+	private Node previous;
+
+	@Override
+	public Object getWert() {
+		return wert;
+	}
+
+	@Override
+	public void setWert(Object wert) {
+		this.wert = (T) wert;
+	}
+
+	@Override
+	public Node getNext() {
+		return next;
+	}
+
+	@Override
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	@Override
+	public Node getPrevious() {
+		return previous;
+	}
+
+	@Override
+	public void setPrevious(Node prev) {
+		this.previous = prev;
+	}
+}
+
+class Uebung {
+	public static void main(String[] args) {
+		Nodeimpl<String> node1 = new Nodeimpl<>();
+		Nodeimpl<String> node2 = new Nodeimpl<>();
+
+		node1.setWert("text1");
+		node1.setNext(node2);
+
+		node2.setWert("text2");
+		node2.setPrevious(node1);
+
+		System.out.println(node1.getWert());
+		System.out.println(node1.getNext().getWert());
+	}
+}
